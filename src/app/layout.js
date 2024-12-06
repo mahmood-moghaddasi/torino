a00550abc67c3a08941467eed4e093bc3974f570
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-12-06 20:28:04 +0330

    stup tailwind config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
+import Header from "@/components/templates/Header";
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
