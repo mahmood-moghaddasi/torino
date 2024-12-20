32e2e7cf1385eee00384df9dadd07c58cb2372e9
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-12-20 16:27:11 +0330

    🔒Authorization handled

--- src/app/profile/layout.js
+++ src/app/profile/layout.js
@@ -1,22 +1,25 @@
 import Header from "@/components/templates/Header";
+import AuthProvider from "@/provider/AuthProvider";
 import React from "react";
 
 function ProfileLayout({ children }) {
   return (
-    <div className="flex pt-9 pr-[126px] pl-[126px] gap-[18px] mb-12">
-      <div className=" h-[170px] divide-y  border  w-auto rounded-[10px] overflow-hidden">
-        <div className="w-[284px] pt-5 pb-4 pr-3 bg-primary-color/25">
-          <p>پروفایل</p>
+    <AuthProvider>
+      <div className="flex pt-9 pr-[126px] pl-[126px] gap-[18px] mb-12">
+        <div className=" h-[170px] divide-y  border  w-auto rounded-[10px] overflow-hidden">
+          <div className="w-[284px] pt-5 pb-4 pr-3 bg-primary-color/25">
+            <p>پروفایل</p>
+          </div>
+          <div className="w-[284px] pt-4 pb-4 pr-3">
+            <p>تور های من </p>
+          </div>
+          <div className="w-[284px] pt-4 pb-5 pr-3 bg-primary-color/25">
+            <p>تراکنش های من</p>
+          </div>
         </div>
-        <div className="w-[284px] pt-4 pb-4 pr-3">
-          <p>تور های من </p>
-        </div>
-        <div className="w-[284px] pt-4 pb-5 pr-3 bg-primary-color/25">
-          <p>تراکنش های من</p>
-        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </AuthProvider>
   );
 }
 
---
+++ src/provider/AuthProvider.js
@@ -0,0 +1,24 @@
+"use client";
+import { useGetUserData } from "@/services/queries";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+function AuthProvider({ children }) {
+  const router = useRouter();
+  const { isPending, data } = useGetUserData();
+
+  useEffect(() => {
+    if (!isPending && !data?.data) router.push("/");
+  }, [isPending]);
+
+  if (isPending)
+    return (
+      <p className=" m-auto bg-blue-100 text-blue-500 w-fit animate-ping">
+        Loading...
+      </p>
+    );
+
+  return children;
+}
+
+export default AuthProvider;
